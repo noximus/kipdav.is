2eb4ce956a1c6d45b0acc8bee8c0b91dc7422653
noximus <hrichardson83@gmail.com>
2015-07-26 21:12:54 -0400

    init - Yoeman Angular Build - Contact route added

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name kipdavisApp
+ * @description
+ * # kipdavisApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('kipdavisApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kipdavisApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the kipdavisApp
+ */
+angular.module('kipdavisApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
